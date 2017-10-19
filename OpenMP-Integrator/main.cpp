89740754e25488081f1b3aad4c5acfaa42f2d353
Kaustubh Shivdikar <kaustubhcs07@gmail.com>
2017-10-18 22:35:57 -0400

    Functional Xcode Update

--- OpenMP-Integrator/main.cpp
+++ OpenMP-Integrator/main.cpp
@@ -7,9 +7,13 @@
 //
 
 #include <iostream>
+#include "omp.h"
+
+using namespace std;
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    cout << "Hello, World!\n";
+    return 1;
 }
